21f0d74fa0ae399ca641692f028db772a013f599
dschep <dschep@gmail.com>
2024-07-02 22:45:05 +0000

    Deploying to gh-pages from @ MapRVA/open-landmarks@a8587fd1c6635caff15676cde0bd8424aa5d7af2 🚀

---
+++ README.md
@@ -0,0 +1,2 @@
+# Open Landmarks
+A cartographic experiment using landmarks to help users orient themselves.
---
+++ build.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import osmtogeojson from "osmtogeojson";
+
+fs.writeFileSync(
+  "landmarks.geojson",
+  JSON.stringify({
+    type: "FeatureCollection",
+    features: await Promise.all(
+      JSON.parse(fs.readFileSync("landmarks.json")).map(async (inp) => {
+        const resp = await fetch(
+          `https://overpass-api.de/api/interpreter?data=[out:json];nwr[wikidata="${inp.wikidata}"]${inp.extra_qualifier || ""};out center;`,
+        );
+        const {
+          features: [feature],
+        } = osmtogeojson(await resp.json());
+        feature.properties.maki_temaki = inp.maki_temaki;
+        return feature;
+      }),
+    ),
+  }),
+);
