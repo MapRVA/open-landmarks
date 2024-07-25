6da99de40ba63392568de0d840cc15c302a8691e
dschep <dschep@gmail.com>
2024-07-25 21:25:36 +0000

    Deploying to gh-pages from @ MapRVA/open-landmarks@72981f2294eb5dd55fc02f24a62afc42f9e457a4 🚀

--- build.js
+++ build.js
@@ -1,20 +1,12 @@
 import fs from "fs";
 import osmtogeojson from "osmtogeojson";
 
-fs.writeFileSync(
-  "landmarks.geojson",
-  JSON.stringify({
-    type: "FeatureCollection",
-    features: await Promise.all(
-      JSON.parse(fs.readFileSync("landmarks.json")).map(async (inp) => {
-        const resp = await fetch(
-          `https://overpass-api.de/api/interpreter?data=[out:json];nwr[wikidata="${inp.wikidata}"]${inp.extra_qualifier || ""};out center;`,
-        );
-        const {
-          features: [feature],
-        } = osmtogeojson(await resp.json());
-        return { ...feature, properties: { ...feature.properties, ...inp } };
-      }),
-    ),
-  }),
-);
+const features = [];
+for (const inp of JSON.parse(fs.readFileSync("landmarks.json"))) {
+  const resp = await fetch(
+    `https://overpass-api.de/api/interpreter?data=[out:json];nwr[wikidata="${inp.wikidata}"]${inp.extra_qualifier || ""};out center;`,
+  );
+  const { features: [feature] } = osmtogeojson(await resp.json());
+  features.push({ ...feature, properties: { ...feature.properties, ...inp } });
+};
+fs.writeFileSync("landmarks.geojson", JSON.stringify({ type: "FeatureCollection", features }));
